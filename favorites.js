2d5a4efbb3237a52fa82b7eaa74605f4a79873dc
Vidya <vidyaashree.n@gmail.com>
2023-12-07 12:32:32 +0530

    working of favorites button and home button

---
+++ favorites.js
@@ -0,0 +1,8 @@
+
+    function openFavoritesPage() {
+        window.location.href = './favorites.html';
+    }
+    function openHomePage() {
+        window.location.href = './index.html';
+    }
+
